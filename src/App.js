f20d4595f4045200015e1acd524d0d3afb9122c6
balkanskiy <balkanskiy.alexey@gmail.com>
2021-07-30 18:15:43 +0200

    Add deploy information to README.md

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import Header from "./components/header";
 import "./App.css";
 
 function App() {
+  /* For Data fetching */
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+
   return (
     <div className="App">
       <Header />
