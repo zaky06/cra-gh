962e9bb13a9cfcef724a3d75ae04e88033845c9b
zaky06 <orochimaruimba@gmail.com>
2021-08-20 00:52:52 +0300

    Test commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import Header from "./components/header/header";
 import "./App.css";
-import PropTypes from 'prop-types';
+
 
 const App = (props) => {
   App.propTypes = {
