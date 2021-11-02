35b932e882ae3aa060aa6863f904f38183308aee
zaky06 <orochimaruimba@gmail.com>
2021-11-02 21:40:11 +0300

    work done

--- pen-array.js
+++ pen-array.js
@@ -30,8 +30,11 @@ const testData = [
 function getDirectory(users) {
   // Add answer here
   const newUser = users.map(item => {
-    // Object.value(item.name) = Object.keys(item.name) && (Object.values(item.name) = Object.values(item.email));
-    (item.name.value = item.name.key) && (item.name.value = item.email.value);
+    delete item.age;
+    const keyName = item.name;
+    item[keyName] = item.email;
+    delete item.name;
+    delete item.email;
     return item;
   });
   // console.log(item);
