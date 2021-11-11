b30c0d053c800541af3ca7ff83e67c0e633a7893
zaky06 <orochimaruimba@gmail.com>
2021-11-11 21:17:46 +0300

    add not working dropdown

--- src/components/miniprofile/dropdown/dropdown.jsx
+++ src/components/miniprofile/dropdown/dropdown.jsx
@@ -1,15 +1,27 @@
-import { props } from 'bluebird';
 import React from 'react';
 import style from './dropdown.module.css';
 
+
+
 const Dropdown = () => {
     return (
-        <nav className="navbar">
-            <ul className="navbar-nav">
-                {props.children}
-            </ul>
-        </nav>
+        <div className={style.action}>
+            <div className={style.profile} onClick={menuToggle()}>
+              <img className={style.icon} src="https://icon-library.com/images/drop-down-arrow-icon/drop-down-arrow-icon-25.jpg"/>   
+            </div>
+            <div className={style.menu}>
+                <h3>Online Status</h3>
+            </div>
+        </div>    
     )
+    function menuToggle() {
+        const toggleMenu = document.querySelector('.menu');
+        toggleMenu.classList.toggle('active');
+    }
 }
 
 export default Dropdown;
+
+{/* <span>
+    <img className={style.icon} src="https://icon-library.com/images/drop-down-arrow-icon/drop-down-arrow-icon-25.jpg"/>
+</span> */}
--- src/components/miniprofile/miniprofile.jsx
+++ src/components/miniprofile/miniprofile.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import style from './miniprofile.module.css';
+import Dropdown from "./dropdown/dropdown";
 
 const Miniprofile = () => {
     return (
         <div className={style.container}>
             <img className={style.avatar} src="https://gambolthemes.net/workwise-new/images/resources/user.png"/>
             <a className={style.name} href="#">John</a>
-            <span>
-                <img className={style.icon} src="https://icon-library.com/images/drop-down-arrow-icon/drop-down-arrow-icon-25.jpg"/>
-            </span>
+            <Dropdown />
         </div>
     )
 }
