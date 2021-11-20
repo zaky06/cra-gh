d3e933f2c018a22cf2071d8c913a6c3689b6f469
zaky06 <orochimaruimba@gmail.com>
2021-11-20 19:31:49 +0300

    working dropdown

--- src/components/miniprofile/dropdown/dropdown.jsx
+++ src/components/miniprofile/dropdown/dropdown.jsx
@@ -1,27 +1,37 @@
-import React from 'react';
-import style from './dropdown.module.css';
-
-
+import React, { useState } from "react";
+import style from "./dropdown.module.css";
 
 const Dropdown = () => {
-    return (
-        <div className={style.action}>
-            <div className={style.profile} onClick={menuToggle()}>
-              <img className={style.icon} src="https://icon-library.com/images/drop-down-arrow-icon/drop-down-arrow-icon-25.jpg"/>   
-            </div>
-            <div className={style.menu}>
-                <h3>Online Status</h3>
-            </div>
-        </div>    
-    )
-    function menuToggle() {
-        const toggleMenu = document.querySelector('.menu');
-        toggleMenu.classList.toggle('active');
-    }
-}
+  const [isMenuOpened, setIsmenuOpened] = useState(false);
+
+  function menuToggle() {
+    setIsmenuOpened(!isMenuOpened);
+
+    // const toggleMenu = document.querySelector('.menu');
+    // toggleMenu.classList.toggle('active');
+  }
+
+  return (
+    <div className={style.action}>
+      <div className={style.profile} onClick={menuToggle}>
+        <img
+          className={style.icon}
+          src="https://icon-library.com/images/drop-down-arrow-icon/drop-down-arrow-icon-25.jpg"
+        />
+      </div>
+      {isMenuOpened && (
+        <div className={style.menu}>
+          <h3>Online Status</h3>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Dropdown;
 
-{/* <span>
+{
+  /* <span>
     <img className={style.icon} src="https://icon-library.com/images/drop-down-arrow-icon/drop-down-arrow-icon-25.jpg"/>
-</span> */}
+</span> */
+}
---
+++ src/test.js
@@ -0,0 +1,4 @@
+const first = "<div>test1</div>";
+const second = "<div>test2</div>";
+
+console.log(first && second);
