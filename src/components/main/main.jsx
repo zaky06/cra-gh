82b534317b8207e1db5ef3c5bdc370d96552027d
zaky06 <orochimaruimba@gmail.com>
2021-12-09 20:58:47 +0300

    card profileinfo design

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -4,7 +4,7 @@ import style from "./main.module.css";
 
 const Main = () => {
     return (
-        <div className={style.mainn}>
+        <div className={style.main}>
             <ProfileInfo />
         </div>
     )
