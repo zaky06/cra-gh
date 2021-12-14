71530086b17a1627ab2e8fb8fc035e53df176b48
zaky06 <orochimaruimba@gmail.com>
2021-12-15 00:37:31 +0300

    style main

--- src/components/actionpost/actionpost.jsx
+++ src/components/actionpost/actionpost.jsx
@@ -7,7 +7,7 @@ const ActionPost = () => {
     return (
         <div className={style.container}>
             <div className={style.postImg}>
-                <img src="https://gambolthemes.net/workwise-new/images/resources/user.png" alt="" />
+                <img src="https://gambolthemes.net/workwise-new/images/resources/user-pic.png" alt="" />
             </div>
             <div className={style.button1}>
               <Button variant="danger" disabled>Post a Project</Button>
