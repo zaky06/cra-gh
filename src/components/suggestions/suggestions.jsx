0f0b2b5e0a206152ad4a4b7ab5225fc29c4d5b75
zaky06 <orochimaruimba@gmail.com>
2021-12-22 22:44:15 +0300

    fix main design

--- src/components/suggestions/suggestions.jsx
+++ src/components/suggestions/suggestions.jsx
@@ -4,7 +4,12 @@ import style from "./suggestions.module.css";
 const Suggestions = () => {
     return (
         <div className={style.suggestions}>
-            <p>Suggestions</p>
+            <div className={style.firstSection}>
+                <h3 className={style.name}>Suggestions</h3>
+                <svg className={style.iconSvg} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"  viewBox="0 0 16 16">
+                <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
+                </svg>
+            </div>
         </div>
     )
 }
